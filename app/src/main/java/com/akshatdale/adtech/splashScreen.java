0dc9515bed28b6c940deef68c94035239f3c8668
Akshat dale <akshatdale1611@gmail.com>
2022-08-06 16:01:50 +0530

    updation in logo,textview,splash screen etc

--- app/src/main/java/com/akshatdale/adtech/splashScreen.java
+++ app/src/main/java/com/akshatdale/adtech/splashScreen.java
@@ -5,23 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class splashScreen extends AppCompatActivity {
 
-    TextView textViewAD;
+//    TextView textViewAD;
+    ImageView imageViewlogo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        textViewAD = findViewById(R.id.textViewAD);
-        textViewAD.setAlpha(0);
+//        textViewAD = findViewById(R.id.textViewAD);
+//        textViewAD.setAlpha(0);
+
+        imageViewlogo = findViewById(R.id.imageViewlogo);
+        imageViewlogo.setAlpha(0f);
+
 
         Intent intent = new Intent(splashScreen.this,MainActivity.class);
 
 
-        textViewAD.animate().alpha(1).setDuration(3000);
+//        textViewAD.animate().alpha(1).setDuration(3000);
+        imageViewlogo.animate().alpha(1).setDuration(3000);
 
         new Handler().postDelayed(new Runnable() {
             @Override
